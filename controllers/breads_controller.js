9fa79edbff9d5440f1e9189688897ce634a4f828
Jake Lira <llira09@gmail.com>
2022-12-20 15:09:54 -0600

    part6 solution

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -29,6 +29,15 @@ breads.get('/:arrayIndex', (req, res) => {
     }
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+        bread: Bread[req.params.indexArray],
+        index: req.params.indexArray
+    })
+})
+
+
 
 // CREATE
 breads.post('/', (req, res) => {
@@ -43,6 +52,18 @@ breads.post('/', (req, res) => {
     Bread.push(req.body)
     res.redirect('/breads')
 })
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
 
 // DELETE
 breads.delete('/:indexArray', (req, res) => {
